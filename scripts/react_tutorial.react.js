f5d7596d13b51862a80cea3116b38f822db7d4dc
novy154 <slnowak154@gmail.com>
2015-02-21 13:35:36 +0100

    Static view

--- scripts/react_tutorial.react.js
+++ scripts/react_tutorial.react.js
@@ -1,17 +1,91 @@
 'use strict'
 
+var teams = [
+  {
+    name: "Real Madrid",
+    rating: "GOOD"
+  },
+
+  {
+    name: "Manchester United",
+    rating: "GOOD"
+  },
+
+  {
+    name: "Real Madrid Castilla",
+    rating: "POOR"
+  },
+
+  {
+    name: "Everton",
+    rating: "AVERAGE"
+  }
+];
+
 
 var FilterableTeamTable = React.createClass({
+  getInitialState: function() {
+    return {
+      filterText: ''
+    };
+  },
+
   render: function() {
     return (
       <div>
-        hello
+        <SearchBar/>
+        <TeamTable teams={this.props.teams}/>
       </div>
     );
   }
 
 });
 
+var TeamTable = React.createClass({
+  render: function() {
+    var rows = this.props.teams.map(function(team) {
+      return <TeamRow key={team.name} team={team}/>;
+    });
+
+    return (
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Rating</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {rows}
+          </tbody>
+        </table>
+
+      </div>
+    );
+  }
+});
+
+var TeamRow = React.createClass({
+  render: function() {
+    return (
+      <tr>
+        <td>{this.props.team.name}</td>
+        <td>{this.props.team.rating}</td>
+      </tr>
+    );
+  }
+});
+
+var SearchBar = React.createClass({
+  render: function() {
+    return (
+      <input type="text" placeholder="Filter by name" ref="filterText" />
+    );
+  }
+});
+
 React.render(
-  <FilterableTeamTable />, document.body
+  <FilterableTeamTable teams={teams}/>, document.body
 );
